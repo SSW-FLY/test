0161e47148df69f696010a9b9fba1fd963bcbe81
yangcheng <cheng.yang@mail.hypers.com>
2021-01-21 16:47:04 +0800

    修改

--- src/main/java/itcode/Application.java
+++ src/main/java/itcode/Application.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author imp
+ * @date 2021/1/21
  */
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
     }
 }
