8d36a565f0386d718778f894873c3f91c85d5854
yangcheng <cheng.yang@mail.hypers.com>
2021-02-02 14:56:57 +0800

    test

--- prometheus/src/test/java/itcode/Test.java
+++ prometheus/src/test/java/itcode/Test.java
@@ -1,9 +1,25 @@
 package itcode;
 
+import org.joda.time.DateTime;
+
 /**
  * @author imp
  * @date 2021/1/27
  */
 public class Test {
 
+    @org.junit.Test
+    public void test1() {
+        DateTime now = DateTime.now();
+        DateTime dateTime = now.withSecondOfMinute(0);
+        DateTime dateTime1 = now.withTimeAtStartOfDay();
+        DateTime dateTime2 = now.withHourOfDay(0);
+        DateTime dateTime3 = now.withMinuteOfHour(0);
+        System.out.println(now);
+        System.out.println(dateTime);
+        System.out.println(dateTime1);
+        System.out.println(dateTime2);
+        System.out.println(dateTime3);
+        System.out.println(now.getHourOfDay());
+    }
 }
